a824d7440571f1aff1140c148cafffa8504d1e4b
Anssi Gröhn <anssi.grohn@karelia.fi>
2013-10-09 17:38:45 +0300

    Added MainScene with simple GUI

---
+++ Assets/Scripts/MainGUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class MainGUI : MonoBehaviour {
+
+	void OnGUI()
+	{
+		GUILayout.BeginHorizontal();
+		GUILayout.Label ("Hello world!");
+		GUILayout.EndHorizontal();
+	}
+}
